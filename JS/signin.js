a0cb034a3fef2daade26bb1f38f37468d7a0c1fc
01yuta24 <111856668+01yuta24@users.noreply.github.com>
2024-04-13 23:00:11 +0900

    Update signin.js

--- JS/signin.js
+++ JS/signin.js
@@ -1,13 +1,20 @@
-function signUpFn(){
-  const username = document.querySelector("#username-field").value;
-  const password = document.querySelector("#password-field").value;
-  console.log(username,password);
-
-  localStorage.setItem("username",username);
-
-  window.location.href = "index.html";
-}
-
+window.addEventListener("load",function(){
+  const username = document.querySelector("#username-field");
+  const password = document.querySelector("#password-field");
+  const errorEl = document.querySelector(".error");
+  
+  function signUpFn(){
+    if (username.value && password.value){
+      localStorage.setItem("username",username.value);
+      window.location.href = "index.html";
+    } else {
+      username.value = "";
+      password.value = "";
+      errorEl.innerHTML = "正しく入力を行ってください";
+      return false;
+    }
+  }
+  
   const btn = document.querySelector(".signin-btn");
-
   btn.addEventListener("click",signUpFn);
+});
